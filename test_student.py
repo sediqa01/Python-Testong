1274523b6e3361508aee84524679fa3cbf0f1c1d
Sediqa <sadat.hadid2000@gmail.com>
2023-01-30 13:51:00 +0000

    Create a test methode  for student emails

--- test_student.py
+++ test_student.py
@@ -1,11 +1,22 @@
 import unittest
 from student import Student
 
+
 class TestStudent(unittest.TestCase):
 
     def test_full_name(self):
         student = Student('Sediqa', 'Hadid')
         self.assertEqual(student.full_name, 'Sediqa Hadid')
+
+    def test_alert_santa(self):
+        student = Student('Sediqa', 'Hadid')
+        student.alert_santa()
+
+        self.assertTrue(student.naughty_list)
+
+    def test_email_address(self):
+        student = Student('Sediqa', 'Hadid')
+        self.assertEqual(student.email_address, 'Sediqa.Hadid@email.com')
 
 
 if __name__ == "__main__":
