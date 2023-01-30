dd594f1629567a4cd0ea332685209ef186745f0d
Sediqa <sadat.hadid2000@gmail.com>
2023-01-30 20:53:48 +0000

    Create test fot student_schedule and use Moking to test successful and failed responses

--- student.py
+++ student.py
@@ -1,7 +1,9 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -14,13 +16,21 @@ class Student:
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
 
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
     def alert_santa(self):
         self.naughty_list = True
 
-    @property
-    def email_address(self):
-        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
-
-    
     def apply_extension(self, days):
         self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
--- test_student.py
+++ test_student.py
@@ -1,17 +1,17 @@
 import unittest
 from student import Student
-from datetime import date, timedelta
-
+from datetime import timedelta
+from unittest.mock import patch
 
 class TestStudent(unittest.TestCase):
-
+    
     @classmethod
     def setUpClass(cls):
         print("set up class")
 
     def setUp(self):
         print("setup")
-        self.student = Student("Sediqa", "Hadid")
+        self.student = Student("John", "Doe")
 
     @classmethod
     def tearDownClass(cls):
@@ -22,21 +22,42 @@ class TestStudent(unittest.TestCase):
 
     def test_full_name(self):
         print("test_full_name")
-        self.assertEqual(self.student.full_name, "Sediqa Hadid")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
         print("test_alert_santa")
         self.student.alert_santa()
         self.assertTrue(self.student.naughty_list)
 
-    def test_email_address(self):
-        print("test_email_address")
-        self.assertEqual(self.student.email_address, 'sediqa.hadid@email.com')
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
     def test_apply_extension(self):
         old_end_date = self.student.end_date
         self.student.apply_extension(5)
         self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+        
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+            
+    def test_course_schedule_failed(self):
+        """
+        In the path "student" comes from the name of the file student.py
+        If you have named the file something else - it will need to match
+        eg, students.py would need "students.requests.get"
+        """
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong")
+            
 
 if __name__ == "__main__":
     unittest.main()
